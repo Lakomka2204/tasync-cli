141606dc933f51ae89d92e02d3c2cd2c9e1c85b7
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 17:35:57 +0300

    rename

---
+++ AppConfig.cs
@@ -0,0 +1,31 @@
+namespace Tasync
+{
+    public static class Config
+    {
+        public static string? UserToken
+        {
+            get
+            {
+                using var sr = new StreamReader(Path.Combine(UserDir, UserConfigFile));
+                return sr.ReadLine()?.Trim();
+            }
+            set
+            {
+                if (value is not null)
+                {
+                    using var sw = new StreamWriter(Path.Combine(UserDir, UserConfigFile));
+                    sw.Write(value);
+                }
+                else File.Delete(Path.Combine(UserDir, UserConfigFile));
+            }
+        }
+        public const string UserConfigFile = ".gtasync";
+        public static string UserDir
+        {
+            get
+            {
+                return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            }
+        }
+    }
+}
