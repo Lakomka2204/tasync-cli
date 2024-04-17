78b5560890f539cd8170cb3fb46121e2fc2d02a5
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 18:23:39 +0300

    added parser for parameters + added non-zero exit code if none of commands is executed

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿using CommandLine;
 using Tasync.Commands;
+using Tasync.Utils;
 
+var config = Parser.Default.ParseArguments<CLIOptions>(args).Value;
 var commandType = typeof(ICommand);
 var commandTypes = AppDomain.CurrentDomain.GetAssemblies()
     .SelectMany(x => x.GetTypes())
     .Where(x => commandType.IsAssignableFrom(x) && x != commandType).ToArray();
-await Parser.Default.ParseArguments(args,commandTypes).WithParsedAsync<ICommand>(async (command) => await command.Execute());
+var parser = await Parser.Default
+.ParseArguments(args,commandTypes)
+.WithParsedAsync<ICommand>(async (command) => await command.Execute(config));
+await parser.WithNotParsedAsync(errors => {
+    if (errors.Select(x => x.StopsProcessing).Any())
+    Environment.ExitCode = 1;
+    return Task.CompletedTask;
+});
