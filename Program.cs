46f1f3a471d97c5faec76df4a070f23303a680dc
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 11:15:47 +0300

    added url param

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-﻿string appName = AppDomain.CurrentDomain.FriendlyName;
+﻿namespace tasync;
+string appName = AppDomain.CurrentDomain.FriendlyName;
+string host = Environment.GetEnvironmentVariable("T_HOST") ?? "192.168.31.203";
+string dir = Directory.GetCurrentDirectory();
 string help = $@"
-usage: {appName} [command]
+usage: {appName} [parameters] [command]
+
+parameters:
+-host [host] - change default host url which is: {host} to specified
+-dir [dir] - change working directory to specified
 
 commands:
 login [username] [password] - login to the cloud
@@ -13,7 +20,6 @@ commits - get list of all commits of current folder
 commits [folder name] - get list of all commits of specified cloud folder
 ";
 
-string host = Environment.GetEnvironmentVariable("T_HOST") ?? "192.168.31.203";
 
 if (args.Length == 0)
 {
@@ -21,14 +27,45 @@ if (args.Length == 0)
   return 0;
 }
 
+#region Params secion
+var commandEnum = args.ToList().GetEnumerator() as IEnumerator<string>;
+while (commandEnum.MoveNext())
+{
+  var current = commandEnum.Current;
+  switch (current)
+  {
+    case "-dir":
+      if (!commandEnum.MoveNext())
+      {
+        Console.WriteLine(help);
+        return 1;
+      }
+      var newDir = commandEnum.Current;
+      if (!Directory.Exists(Path.Combine(dir, newDir)))
+      {
+        Console.WriteLine("dir {0} does not exists", newDir);
+        return 1;
+      }
+      dir = newDir;
+      break;
+      case "-url":
+      if (!commandEnum.MoveNext())
+      {
+        Console.WriteLine(help);
+        return 1;
+      }
+      host = commandEnum.Current;
+  }
+}
+commandEnum.Reset();
+#endregion
+
 var commandEnum = args.ToList().GetEnumerator() as IEnumerator<string>;
 while (commandEnum.MoveNext()) {
   string command = commandEnum.Current;
   switch(command) {
     case "login":
-      using (var h = new HttpClient()) {
-        
-      }
+      
     break;
   }
 }
