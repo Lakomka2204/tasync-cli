1a94c2881a0ec3fec45ba06b3057767e0a0dc236
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 18:24:08 +0300

    implemented list of folders

--- commands/ListCommand.cs
+++ commands/ListCommand.cs
@@ -1,14 +1,32 @@
 
+using System.Net.Http.Json;
 using CommandLine;
+using Tasync.Responses;
+using Tasync.Utils;
 
 namespace Tasync.Commands
 {
     [Verb("list", HelpText = "List all folders stored in the cloud")]
     public class ListCommand : ICommand
     {
-        public async Task Execute()
+        public async Task Execute(CLIOptions options)
         {
-            throw new NotImplementedException();
+            if (Config.UserToken is null)
+            {
+                Console.WriteLine("You are not logged in");
+                Environment.ExitCode = 1;
+                return;
+            }
+            var uri = Request.ComposeUri(options.Host,"/folder");
+            var res = await Request.Make(HttpMethod.Get,uri);
+            if (!res.IsSuccessStatusCode)
+            {
+                var error = await res.Content.ReadFromJsonAsync<ErrorResponse>();
+                Environment.ExitCode = Request.PrintHttpErrorAndExit(error);
+                return;
+            }
+            var folders = await res.Content.ReadFromJsonAsync<FolderResponse[]>();
+            Console.WriteLine(string.Join(Environment.NewLine,folders?.Select(x => x.ToString()) ?? ["None"]));
         }
     }
 }
