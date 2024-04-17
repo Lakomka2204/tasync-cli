71f6c4a8cc4eb18e5995f158909edbd5a9cb6f73
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 21:56:25 +0300

    optimized some properties

--- utils/AppConfig.cs
+++ utils/AppConfig.cs
@@ -2,21 +2,28 @@ namespace Tasync.Utils
 {
     public static class Config
     {
+        public static string Path {
+            get {
+                return System.IO.Path.Combine(UserDir, UserConfigFile);
+            }
+        }
         public static string? UserToken
         {
             get
             {
-                using var sr = new StreamReader(Path.Combine(UserDir, UserConfigFile));
+                if (!File.Exists(Path))
+                    return null;
+                using var sr = new StreamReader(Path);
                 return sr.ReadLine()?.Trim();
             }
             set
             {
                 if (value is not null)
                 {
-                    using var sw = new StreamWriter(Path.Combine(UserDir, UserConfigFile));
+                    using var sw = new StreamWriter(Path);
                     sw.Write(value);
                 }
-                else File.Delete(Path.Combine(UserDir, UserConfigFile));
+                else File.Delete(Path);
             }
         }
         public const string UserConfigFile = ".gtasync";
