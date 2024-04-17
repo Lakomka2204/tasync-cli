566b5dc8500f22bf3ab8352eb88240808a74bff6
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 18:23:48 +0300

    added force option to commit

--- commands/CommitCommand.cs
+++ commands/CommitCommand.cs
@@ -1,11 +1,14 @@
 using CommandLine;
+using Tasync.Utils;
 
 namespace Tasync.Commands
 {
   [Verb("commit", HelpText = "Commits to the folder or creates new if doesn't exist")]
   public class CommitCommand : ICommand
   {
-    public async Task Execute()
+    [Option('f',"force",HelpText = "Force commit into cloud", Default = false)]
+    public bool Force {get; set;} = false;
+    public Task Execute(CLIOptions options)
     {
       throw new NotImplementedException();
     }
