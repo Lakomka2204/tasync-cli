9b40fe4929eb29df852c81a606de2d56578e78b4
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 17:36:59 +0300

    added composing uri and printing http errors from varying messages array or single string

--- Request.cs
+++ Request.cs
@@ -1,11 +1,14 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text.Json;
+using Tasync.Responses;
 
-namespace tasync
+namespace Tasync
 {
   public static class Request
   {
     public const string AuthScheme = "Bearer";
+    public const string ErrorPrefix = "HTTP Error {0}";
     /// <summary>
     /// wrapped request
     /// </summary>
@@ -14,13 +17,41 @@ namespace tasync
     /// <param name="data">new {a="3",b=true,c=3.54}</param>
     /// <param name="auth">raw header without bearer</param>
     /// <returns>http result</returns>
-    public static async Task<HttpResponseMessage> Make(HttpMethod method, string url, object? data,string? auth)
+    public static async Task<HttpResponseMessage> Make(HttpMethod method, Uri url, object? data, string? auth = null)
     {
+
       using var http = new HttpClient();
-      var content = JsonContent.Create(data);
       http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(AuthScheme, auth);
-      var req = new HttpRequestMessage(method, url) { Content = content };
+      var req = new HttpRequestMessage(method, url);
+      if (data is not null)
+        req.Content = JsonContent.Create(data);
+      // req.Content.ReadAsStream().CopyTo(Console.OpenStandardError());
       return await http.SendAsync(req);
+    }
+    public static Uri ComposeUri(string scheme,string baseUrl, string path, string? query = null)
+    {
+      return new UriBuilder()
+      {
+        Scheme = scheme,
+        Host = baseUrl,
+        Path = path,
+        Query = query
+      }.Uri;
+    }
+    public static int PrintHttpErrorAndExit(ErrorResponse? error)
+    {
+      if (error is null) return 1;
+      if (error.Message?.ToString() is string singleError)
+      {
+        Console.WriteLine(ErrorPrefix, singleError);
+        return 1;
+      }
+      if (error.Message is JsonElement arrayError && arrayError.ValueKind == JsonValueKind.Array)
+      {
+        Console.WriteLine(ErrorPrefix,string.Join('\n',arrayError.EnumerateArray()));
+        return 1;
+      }
+      return -1;
     }
   }
 }
