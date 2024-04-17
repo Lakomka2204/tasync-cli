ec175a16cab5cfe87634bcc6fda8fd38fe6e661e
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-17 21:56:35 +0300

    half-implemented archive class

--- utils/Archive.cs
+++ utils/Archive.cs
@@ -1,7 +1,14 @@
+using System.IO.Compression;
+
 namespace Tasync.Utils
 {
     public static class Archive
     {
-        
+        public static void ExtractTo(Stream archiveStream, string extractDir)
+        {
+            using var zipArchive = new ZipArchive(archiveStream, ZipArchiveMode.Read,false);
+            zipArchive.ExtractToDirectory(extractDir, true);
+            // todo create .tasync file in /utils/InfoFile.cs...
+        }
     }
 }
