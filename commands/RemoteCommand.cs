13f221631c5f9a16cdbbd2263cc25b006145caf3
Lakomka2204 <denis.kozachok22@gmail.com>
2024-04-18 15:03:31 +0300

    added command to see/change cloud folder name

---
+++ commands/RemoteCommand.cs
@@ -0,0 +1,29 @@
+
+using CommandLine;
+using Tasync.Utils;
+
+namespace Tasync.Commands
+{
+    [Verb("remote",default,["r"],HelpText = "Get or change cloud name")]
+    public class RemoteCommand : BaseCommand
+    {
+        [Value(0,MetaName = "new remote",HelpText = "Sets new cloud folder name")]
+        public string? NewRemote { get; set; }
+        public override async Task Execute()
+        {
+            try{
+                var info = new InfoFile(Dir);
+                if (NewRemote is not null)
+                    info.RemoteFolderName = NewRemote;
+                else
+                    Console.WriteLine(info.RemoteFolderName);
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine("Error: {0}",ex.Message);
+                Environment.ExitCode = ex.HResult;
+            }
+            await Task.CompletedTask;
+        }
+    }
+}
